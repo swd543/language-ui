38dd679b6cb30f7ea142cd9825bbc112b5f23634
Swapneel Datta <33709232+swd543@users.noreply.github.com>
2018-12-12 14:21:11 +0530

    Improvement/code refactor (#1)
    * test change
    
    * Prettier config and refactor

--- src/App.js
+++ src/App.js
@@ -1,55 +1,67 @@
-import { AppBar, Drawer, IconButton, List, ListItem, ListItemText, Toolbar, Typography } from '@material-ui/core';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import MenuRounded from '@material-ui/icons/MenuRounded';
-import React, { Component } from 'react';
-import './App.css';
-import ThemePage from './components/ThemePage';
-import darkTheme from './styles/darkTheme';
+import {
+  AppBar,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Toolbar,
+  Typography
+} from "@material-ui/core";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import MenuRounded from "@material-ui/icons/MenuRounded";
+import React, { Component } from "react";
+import "./App.css";
+import Clap from "./components/Clap";
+import darkTheme from "./styles/darkTheme";
 
-const dummyElements=["Clap", "Poppy", "Macaroni", "Asparagus", "Lounge", "Buga", "Maceron", "Langrange", "Newton", "Einstein"];
+const dummyElements = [
+  "Clap",
+  "Poppy",
+  "Macaroni",
+  "Asparagus",
+  "Lounge",
+  "Buga",
+  "Maceron",
+  "Langrange",
+  "Newton",
+  "Einstein"
+];
 
 class App extends Component {
-  state={
-    drawerOpen:false
-  }
+  state = {
+    drawerOpen: false
+  };
 
-  handleClick=()=>{
-    this.setState(s=>({...s, drawerOpen:!s.drawerOpen}))
-    console.log(this.state)
-  }
+  handleClick = () => {
+    this.setState(s => ({ ...s, drawerOpen: !s.drawerOpen }));
+    console.log(this.state);
+  };
 
-  handleClose=()=>{
-    this.setState(s=>({...s, drawerOpen:false}))
-    console.log(this.state)
-  }
+  handleClose = () => {
+    this.setState(s => ({ ...s, drawerOpen: false }));
+    console.log(this.state);
+  };
 
   render() {
     return (
       <MuiThemeProvider theme={darkTheme}>
-        <AppBar position='sticky'>
-          <Toolbar >
+        <AppBar position="sticky">
+          <Toolbar>
             <IconButton onClick={this.handleClick}>
-              <MenuRounded/>
+              <MenuRounded />
             </IconButton>
-            <Typography variant='h6'>
-              My app
-            </Typography>
+            <Typography variant="h6">My app</Typography>
           </Toolbar>
         </AppBar>
-        {
-          dummyElements.map(v=><ThemePage text={v}/>)
-        }
+        <Clap elements={dummyElements} />
         <Drawer open={this.state.drawerOpen} onClose={this.handleClose}>
           <List>
-            {
-              dummyElements.map(v=>
+            {dummyElements.map(v => (
               <ListItem button>
-                <ListItemText>
-                  {v}
-                </ListItemText>
-              </ListItem>  
-              )
-            }
+                <ListItemText>{v}</ListItemText>
+              </ListItem>
+            ))}
           </List>
         </Drawer>
       </MuiThemeProvider>
--- src/components/ThemePage.js
+++ src/components/ThemePage.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import { Typography } from '@material-ui/core';
-import PropTypes from 'prop-types'
+import React from "react";
+import { Typography } from "@material-ui/core";
+import PropTypes from "prop-types";
 
 const ThemePage = ({ text }) => {
   return (
     <div>
-      <Typography variant='h1' style={{
-        padding:10
-      }}>
+      <Typography
+        variant="h1"
+        style={{
+          padding: 10
+        }}
+      >
         Clap {text}
       </Typography>
     </div>
-  )
-}
+  );
+};
 
-ThemePage.propTypes={
-    text: PropTypes.string,
-    plooga: PropTypes.string
-}
+ThemePage.propTypes = {
+  text: PropTypes.string
+};
 
-export default ThemePage
+export default ThemePage;
